ef51791170dbeb5a4eeda536e400cd9fef131729
Felix Cornelissen <felix@icatt.nl>
2025-03-29 10:37:16 +0100

    custom media

--- samples/MinimalHtml.Sample/postcss.config.cjs
+++ samples/MinimalHtml.Sample/postcss.config.cjs
@@ -1,9 +1,11 @@
 // postcss.config.js
 const postcssJitProps = require('postcss-jit-props');
+const postcssCustomMedia = require('postcss-custom-media')
 const path = require('path');
 
 module.exports = {
   plugins: [
+    postcssCustomMedia(),
     postcssJitProps({
       files: [
         path.resolve(__dirname, 'node_modules/open-props/open-props.min.css'),
