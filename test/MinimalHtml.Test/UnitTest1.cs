e68ef64949dc539b7b47b6ff7daabcd86038b696
Felix Cornelissen <felix@icatt.nl>
2025-08-29 20:49:26 +0200

    add test

---
+++ test/MinimalHtml.Test/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Pipelines;
+
+namespace MinimalHtml.Test
+{
+    public class UnitTest1
+    {
+        static readonly Template<string> s_helloTemplate = static (writer, str) => writer.Html($"Hello {str}");
+
+        static async Task<string> GetWorldAsync()
+        {
+            await Task.Delay(1000);
+            return "world";
+        }
+
+        [Fact]
+        public async Task TestWithAsyncProps()
+        {
+            var result = await RenderToString(static writer => writer.Html($"{(GetWorldAsync(), s_helloTemplate)}"));
+            Assert.Equal("Hello world", result);
+        }
+
+        private static async Task<string> RenderToString(Template template)
+        {
+            var pipe = new Pipe();
+            var result = await template((pipe.Writer, CancellationToken.None));
+            pipe.Writer.Complete();
+            using var streamReader = new StreamReader(pipe.Reader.AsStream());
+            return await streamReader.ReadToEndAsync();
+        }
+    }
+}
