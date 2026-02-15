f67fc595e423699a4e3938bafc5b94903614e621
flixcor <felixcornelissen@gmail.com>
2026-02-15 15:36:33 +0100

    fix service worker

--- samples/MinimalHtml.Sample/vite.config.ts
+++ samples/MinimalHtml.Sample/vite.config.ts
@@ -1,36 +1,50 @@
-import { defineConfig } from 'vite'
-import glob from 'fast-glob'
+import { defineConfig } from "vite";
+import glob from "fast-glob";
 import { readFile, stat } from "fs/promises";
-import { writeFileSync } from 'node:fs';
+import { writeFileSync } from "node:fs";
 //import jit from "lightningcss-jit-props"
 //import { resolve } from "node:path"
 
 const findSrc = /Assets\.[A-Za-z]+:~?\/?([^}]+)}/g;
 
 async function getInputs() {
-  const files = await glob(["./Pages/**/*.cs", "./Components/**/*.cs", "./Layouts/**/*.cs"])
+  const files = await glob([
+    "./Pages/**/*.cs",
+    "./Components/**/*.cs",
+    "./Layouts/**/*.cs",
+  ]);
   const promises = files.map((file) =>
     readFile(file, { encoding: "utf-8" }).then((content) =>
-      [
-        ...content.matchAll(findSrc),
-      ].map(([, group]) => group)
-    )
+      [...content.matchAll(findSrc)].map(([, group]) => group),
+    ),
   );
   const all = await Promise.all(promises);
   const distinct = [...new Set(all.flat())];
-  const exists = (await Promise.all(distinct.map(x => stat(x).then(() => x).catch(() => false as const)))).filter(x => !!x)
+  const exists = (
+    await Promise.all(
+      distinct.map((x) =>
+        stat(x)
+          .then(() => x)
+          .catch(() => false as const),
+      ),
+    )
+  ).filter((x) => typeof x === "string");
 
-  return exists as string[];
+  return exists;
 }
 
+const input = await getInputs();
+
+console.warn(...input);
 export default defineConfig({
-  appType: 'custom',
+  appType: "custom",
 
   css: {
     // transformer: 'lightningcss',
-    transformer: 'postcss',
+    transformer: "postcss",
     modules: {
-      getJSON: (cssFileName, json) => writeFileSync(cssFileName + '.json', JSON.stringify(json))
+      getJSON: (cssFileName, json) =>
+        writeFileSync(cssFileName + ".json", JSON.stringify(json)),
     },
     // lightningcss: {
     //   visitor: jit({
@@ -49,14 +63,18 @@ export default defineConfig({
     rolldownOptions: {
       experimental: {
         chunkImportMap: {
-          baseUrl: '/',
-          fileName: '.vite/importmap.json'
-        }
+          baseUrl: "/",
+          fileName: ".vite/importmap.json",
+        },
       },
-      input: await getInputs(),
+      input,
+      output: {
+        entryFileNames: (c) =>
+          c.name.startsWith("serviceworker") ? "[name]-[hash].js" : "assets/[name]-[hash].js",
+      },
     },
-    outDir: 'wwwroot',
+    outDir: "wwwroot",
     // emptyOutDir: false,
     assetsInlineLimit: -1,
   },
-})
+});
