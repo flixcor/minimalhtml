5b73b330ea2010bbea30acdd9e72aa753306474d
Felix <felixcornelissen@gmail.com>
2026-05-09 18:23:23 +0200

    allow ref struct

--- src/MinimalHtml/Template.cs
+++ src/MinimalHtml/Template.cs
@@ -2,5 +2,5 @@ using System.IO.Pipelines;
 
 namespace MinimalHtml;
 
-public delegate ValueTask<FlushResult> Template<in T>(PipeWriter writer, T context);
+public delegate ValueTask<FlushResult> Template<in T>(PipeWriter writer, T context) where T : allows ref struct;
 public delegate ValueTask<FlushResult> Template(PipeWriter writer);
